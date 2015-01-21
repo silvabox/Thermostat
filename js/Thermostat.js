e0c0f190d5d2b5346cf82d7ad2fbae11f327c27e
Ben Forrest <ben@silvabox.com>
2015-01-21 10:14:27 +0000

    a simpler alternatve using local variables for private functions

--- js/Thermostat.js
+++ js/Thermostat.js
@@ -2,6 +2,7 @@
 // these are defined in the closing function and remain in scope and available to
 // Thermostat, however they are unreachable by any other code.
 
+/*
 var Thermostat = function() {
 	var _somePrivateMethod = function() {
 		return "you can't call _somePrivateMethod directly"
@@ -25,6 +26,33 @@ var Thermostat = function() {
 // Note we call the anonymous closure function to return theClass defined above.
 
 // Note that this is now not possible:
+Thermostat.prototype.anotherCallToPrivateMethod = function() {
+	_somePrivateMethod();
+};
+*/
+
+
+
+// This is a slightly simpler version using a private variable to store the function instead.
+// this differs from the previous example in that every instance will have it's own instance
+// of the _somePrivateMethod function.  In the previous example, there is only one instance
+// of _somePrivateMethod.
+var Thermostat = function(defaultTemp) {
+	var temp = defaultTemp;
+
+	var _somePrivateMethod = function() {
+		return "you can't call _somePrivateMethod directly"
+	};
+
+	this.getTemperature= function() {
+		return temp;
+	};
+
+	this.callPrivateMethod = function() {
+		return _somePrivateMethod();
+	};
+};
+
 Thermostat.prototype.anotherCallToPrivateMethod = function() {
 	_somePrivateMethod();
 };
